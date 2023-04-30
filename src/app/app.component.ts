6fe4908bae3173b53765f578284627411678e110
Eder <guusanito123@gmail.com>
2023-04-30 16:17:06 -0600

    Cuarto_Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,8 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-  title: string = 'Angular_email_CRUD';
+  title: string = 'Angular email CRUD';
+  msg: string = '';
   cards = [ 
     {'name': 'Mago de Hielo', cardType:'Tropa', 'email': 'Mago_Hielo@live.com'},
     {'name': 'Bola de Fuego', cardType:'Hechizo', 'email': 'Bola_de_Fuego@live.com'},
@@ -16,15 +17,23 @@ export class AppComponent {
 ];
 
 
-myValue:Number = 0;
+myValue:number = 0;
 model:any = { };
 model2:any = { };
 
 addCard() : void{
   this.cards.push(this.model);
   this.model = {};
+  alert('Se Agrego la Carta');
 }
-deleteCard():void{
+deleteCard(i = this.cards.length):void{
+  var answer = confirm('estas seguro de querer eliminar la carta?');
+  if(answer == true){
+    this.cards.splice(i,1);
+    alert('Se elimino la Carta');
+  }else {
+    alert('la carta no se elimino')
+  }
 }
 
 editCard(i = this.cards.length): void{
@@ -35,6 +44,17 @@ editCard(i = this.cards.length): void{
 }
 
 updateCard():void{
-  console.log(this.model2)
+  let i = this.myValue;
+  for( let j = 0 ; j <this.cards.length; j++){
+    if(i == j){
+      this.cards[i] = this.model2;
+      this.model2 = {};
+    }
+  }
+  alert('Se actualizo la Carta');
+}
+
+closeAlert(): void{
+  
 }
 }
